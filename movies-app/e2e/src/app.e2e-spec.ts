3439a73f4b7357e78fb61d1ccbed7084ab7d1d96
Daniel Nadel <dnadel95@gmail.com>
2021-10-04 12:38:53 +0300

    bugfixes and typos

--- movies-app/e2e/src/app.e2e-spec.ts
+++ movies-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('calorie-app app is running!');
+    expect(await page.getTitleText()).toEqual('movie-app app is running!');
   });
 
   afterEach(async () => {
